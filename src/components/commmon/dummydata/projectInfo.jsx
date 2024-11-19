4581632f22e1891f7f5b28f2ed5e724ccbcab17c
Kimtaeyeon04 <te4939@naver.com>
2024-11-19 19:05:55 +0900

    예은 언니 머지 전

--- src/components/commmon/dummydata/projectInfo.jsx
+++ src/components/commmon/dummydata/projectInfo.jsx
@@ -159,23 +159,5 @@ export const projectInfo = [
       "admin1"
     ]
   },
-  {
-    projectId: 9,
-    projectTitle: "d",
-    projectOwnerName: "최현혜",
-    ownerEmail: "hhlove@naver.com",
-    description: "d",
-    startDate: "Wed Nov 20 2024 00:00:00 GMT+0900 (한국 표준시)",
-    endDate: "Fri Nov 22 2024 00:00:00 GMT+0900 (한국 표준시)",
-    category: "f",
-    usedLanguage: "d",
-    projectLink: "d",
-    solving: "d",
-    challenge: "d",
-    video: "비디오 없음.",
-    coverImage: "없음.",
-    images: [],
-    logo: "없음.",
-    share: true
-  }
+  
 ];
